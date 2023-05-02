8404927aa775702fb089937fc525f341c34fd0c1
ashishbahar <ashishbahar987@gmail.com>
2023-05-02 16:50:18 +0530

    cganges

--- src/components/Center.jsx
+++ src/components/Center.jsx
@@ -11,15 +11,46 @@ export default class CenterMode extends Component {
       infinite: true,
       centerPadding: "0px",
       slidesToShow: 3,
+      arrows: false,
       adaptiveHeight: false,
       speed: 500,
+      autoplay: true,
+      autoplaySpeed: 1000,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true,
+          },
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          },
+        },
+        {
+          breakpoint: 576,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          },
+        },
+      ],
     };
     return (
-      <section>
-        <Container className=" my-5 py-5 overflow-hidden">
-          <div className=" w-100 h-100 bg- white py-5 ">
-            <Slider {...settings} className=" py-5 px-3  ">
-              <div className=" w-100 h-100">
+      <section className="">
+        <Container className=" my-5 py-5  overflow-hidden">
+          <div className="  h-100 bg- white py-5 ">
+            <Slider
+              {...settings}
+              className=" py-5 ps_12    justify-content-between "
+            >
+              <div className="  h-100">
                 <div className="avatar position-relative slide_box">
                   <img
                     src={women}
